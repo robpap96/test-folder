18930e2b58f7bef9cf59313e9c82d9c9d71b9190
robpap96 <robertopapagni1996@outlook.it>
2023-05-05 12:08:04 +0200

    completata quarta richiesta

--- test rain tonic/quarta_richiesta.js
+++ test rain tonic/quarta_richiesta.js
@@ -1,3 +1,12 @@
 // Dati due array di stringhe (non necessariamente della stessa lunghezza), scrivere una
 // funzione in javascript che restituisca un array con tutte le stringhe presenti
 // esclusivamente solo in uno dei due array.
+
+const firsrArray=['sempre'];
+const secondArray=['spreme'];
+
+function pushInArray(array1,array2) {
+    array1.push(array2);
+    return array1;
+}
+console.log(pushInArray(firsrArray,secondArray));
