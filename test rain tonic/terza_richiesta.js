4a07923c0e063628084aa5008ba92822890026a8
robpap96 <robertopapagni1996@outlook.it>
2023-05-05 12:12:31 +0200

    iniziata terza

--- test rain tonic/terza_richiesta.js
+++ test rain tonic/terza_richiesta.js
@@ -2,3 +2,15 @@
 // una funzione in javascript che restituisca delle coppie randomizzate di elementi
 // dell’array. Ciascun elemento dell’array può comparire in una sola coppia. Se n è dispari, il
 // risultato deve riportare le (n-1)/2 coppie + l’elemento non accoppiato.
+
+const names = [
+    'John',
+    'Mary', 
+    'Paul', 
+    'Joe', 
+    'Roberto'
+];
+
+function randomCouple(array) {
+    
+}
