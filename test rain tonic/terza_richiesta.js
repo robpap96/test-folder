244652d83d3da232c6e0765b52e39758c2663cac
robpap96 <robertopapagni1996@outlook.it>
2023-05-07 12:51:58 +0200

    quasi

--- test rain tonic/terza_richiesta.js
+++ test rain tonic/terza_richiesta.js
@@ -19,8 +19,9 @@ function randomCouple(arrayList) {
 
     const newArray=[];
 
-    while(!arrayList.isEmpty()){
+    while(arrayList.length !== 0){
         let j=0;
+        let k=j+2;
 
         for(let i=0; i<length/2; i++) {
             newArray.push(arrayList[Math.floor(Math.random()*length)]);
@@ -28,7 +29,8 @@ function randomCouple(arrayList) {
             newArray.push(arrayList[Math.floor(Math.random()*length)]);
         }
         couples.push(newArray.slice(j, j+1));
-        j+=2;
+        couples.push(newArray.slice(k, k+1));
+        
     }
 
     return couples;
