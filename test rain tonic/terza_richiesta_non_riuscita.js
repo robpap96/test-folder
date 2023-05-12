3b04bf3de756484fc2f750057589fc4405ef151f
robpap96 <robertopapagni1996@outlook.it>
2023-05-12 16:33:50 +0200

    ok

--- test rain tonic/terza_richiesta_non_riuscita.js
+++ test rain tonic/terza_richiesta_non_riuscita.js
@@ -2,6 +2,8 @@
 // una funzione in javascript che restituisca delle coppie randomizzate di elementi
 // dell’array. Ciascun elemento dell’array può comparire in una sola coppia. Se n è dispari, il
 // risultato deve riportare le (n-1)/2 coppie + l’elemento non accoppiato.
+
+//purtroppo non riesco a capire dove sbaglio
 "use strict";
 
 const Names = [
@@ -16,38 +18,32 @@ const Names = [
 function randomCouple(arrayList) {
 
         let length = arrayList.length;
+        let randomLength = arrayList.length;
         let couples = [];
         let oddCouplesName = '';
 
         while(length !== 0 && length !== 1 ){
-            let firstCouplesName = arrayList[Math.floor(Math.random()*length)];
-            let secondCouplesName = arrayList[Math.floor(Math.random()*length)];
+            let firstCouplesName = arrayList[Math.floor(Math.random()*randomLength)];
+            let secondCouplesName = arrayList[Math.floor(Math.random()*randomLength)];
             let m = couples.length;
 
             if( m === 0 ){
                 if(firstCouplesName !== secondCouplesName){
                     couples.push([firstCouplesName, secondCouplesName]);
                     length = length - 2;
-                    m++;
-                    console.log(couples);
-                    console.log(m);
-
                 } else {
-                    console.log('primo if primo break');
                     break;
                 }
             } else if(m === 1) {
                 for(let i=m-1; i<m; i--){
                     if(couples[i].includes(firstCouplesName) || couples[i].includes(secondCouplesName) ||
                     firstCouplesName === secondCouplesName){
-                        console.log('secondo if primo break');
                         break;
                     }
-                    
+                    break;
                 }
-                console.log('secondo if secondo break');
-
-                break;
+                couples.push([firstCouplesName, secondCouplesName]);
+                length = length - 2;
             } else if(m > 1){
                 for(let i=m-1; i<m; i--){
                     if(couples[i].includes(firstCouplesName) || couples[i].includes(secondCouplesName) || 
@@ -55,19 +51,16 @@ function randomCouple(arrayList) {
                         console.log('terzo if primo break');
                         break;
                     }
+                    break;
                 }
-                console.log('terzo if secondo break');
-                break;
+                couples.push([firstCouplesName, secondCouplesName]);
+                length = length - 2;
             }
-            couples.push([firstCouplesName, secondCouplesName]);
-            length = length - 2;
-            m++;
-            console.log(m);
         }
-        console.log(couples);
+        console.log(length);
         while(length===1){
             console.log('while');
-            oddCouplesName = arrayList[Math.floor(Math.random()*length)];
+            oddCouplesName = arrayList[Math.floor(Math.random()*randomLength)];
             let n = couples.length;
             for(let i=n-1; i<n; i--){
                 if(couples[i].includes(oddCouplesName)){
@@ -77,9 +70,8 @@ function randomCouple(arrayList) {
             couples.push(oddCouplesName);
             length=0;
         }
-        
-        return couples;
-    }
+    return couples;
+}
 
 
 console.log(randomCouple(Names));
